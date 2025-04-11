f41a18d7ca6f8c62020d3c5d9e0236ec9e5a62b7
kanak035 <deepsaurabh4362@gmail.com>
2025-04-11 22:53:48 +0530

    cors fixes

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ const ytRoutes = require("./routes/ytroutes");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://ytctrack.netlify.app"],
+  })
+);
 app.use(helmet());
 app.use(express.json());
 
